a55123c1e92fcd7ac5dbf1b4688438d78f1ea135
Vishal <vishal.khanjan@gmail.com>
2019-09-17 23:22:06 +0530

    update code

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('welcome to express js framework!'))
+app.listen(port, () => console.log(`Node server is listening on port no ${port}!`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => 
+	res.send('welcome to node js framework!')
+)
 
 app.get('/users', function(req, res){
 	console.log('users listing');
-	
-	
 	res.send('this is listing');
 });
